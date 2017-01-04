a9d25a54dca2c00dda77f754bb355948b3530b7c
surongxiang <517530615@qq.com>
2017-01-04 12:33:22 +0800

    add filter method

--- src/assign/main/mainFunc.java
+++ src/assign/main/mainFunc.java
@@ -6,6 +6,8 @@ import com.mongodb.client.MongoCollection;
 import assign.mongo.readData;
 import assign.mongo.filterData;
 import org.bson.Document;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCursor;
 
 public class mainFunc {
 
@@ -15,18 +17,26 @@ public class mainFunc {
 		String dbName = "db3";
 		String collectionName = "trajectory";
 
-		MongoClient mongoClient = new MongoClient(host,port);
+		MongoClient mongoClient = new MongoClient(host, port);
 		MongoDatabase database = mongoClient.getDatabase(dbName);
-		
+
 		if (database != null) {
 			MongoCollection<Document> collection = database.getCollection(collectionName);
 			collection.drop();
-			
+
 			readData readThread1 = new readData("/home/wushi/workspace/mongoTest/data", collection);
 			readThread1.run();
-			
+			System.out.println("原始记录数量：" + collection.count());
 			filterData filterdata = new filterData(collection);
-//			filterdata.filterNumberlowthan24();
+			filterdata.filterNumberlowthan24();
+			filterdata.filterRepeatField();
+
+//			FindIterable<Document> findIterable = collection.find();
+//			MongoCursor<Document> mongoCursor = findIterable.iterator();
+//			while (mongoCursor.hasNext()) {
+//				System.out.println(mongoCursor.next());
+//			}
+			System.out.println("清洗后记录数量：" + collection.count());
 
 		}
 	}
--- src/assign/mongo/filterData.java
+++ src/assign/mongo/filterData.java
@@ -1,38 +1,89 @@
 package assign.mongo;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Arrays;
 import com.mongodb.Block;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.model.Accumulators;
 import org.bson.Document;
+import com.mongodb.client.MongoCursor;
 
-public class filterData{
-	private MongoCollection<Document>  collection;
-	
-	public filterData(MongoCollection<Document>  collection) {
+public class filterData {
+	private MongoCollection<Document> collection;
+	List<Document> documentFilter = new ArrayList<Document>();
+
+	public filterData(MongoCollection<Document> collection) {
 		// TODO Auto-generated constructor stub
 		this.collection = collection;
 	}
 
-
 	Block<Document> printBlock = new Block<Document>() {
-        @Override
-        public void apply(final Document document) {
-            System.out.println(document.toJson());
-        }
-    };
-	
-	public AggregateIterable<Document> filterNumberlowthan24(){
-		AggregateIterable<Document> countCollection = collection.aggregate(
-			      Arrays.asList(
-			              Aggregates.group("$personId", Accumulators.sum("count", 1))
-			      )
-			);
-		countCollection.toCollection();
-		System.out.println(countCollection);
-		return countCollection;
+		@Override
+		public void apply(final Document document) {
+			System.out.println(document.toJson());
+		}
+	};
+
+	public void filterNumberlowthan24() {
+		AggregateIterable<Document> iterableCollection = collection
+				.aggregate(Arrays.asList(Aggregates.group("$personId", Accumulators.sum("sum_time", "$time"))));
+		for (Document d : iterableCollection) {
+			int countOfRecord = (int) d.get("sum_time");
+			if (countOfRecord < 276) {
+				String personid = (String) d.get("_id");
+				Document query = new Document("personId", personid);
+				collection.deleteMany(query);
+			} else {
+				Document doc = new Document("personId", (String) d.get("_id"));
+				documentFilter.add(doc);
+			}
+		}
+		return;
 	}
-	
+
+	public void filterRepeatField() {
+		// FindIterable<Document> findItrable1 = collection
+		// .find(Filvalueters.and(Filters.eq("personId", "00000fe70dfd879c"),
+		// Filters.eq("time", 20)));
+		// MongoCursor<Document> mongoCursor1 = findItrable1.iterator();
+		// int countOfCursor = 0;
+		// while(mongoCursor1.hasNext()){
+		// System.out.println(mongoCursor1.next());
+		// countOfCursor++;
+		// }
+		// System.out.println(countOfCursor);
+		int countOfCursor = 0;
+		for (Document d : documentFilter) {
+			for (int i = 0; i < 24; i++) {
+				countOfCursor = 0;
+				String personid = (String) d.get("personId");
+				Document query1 = new Document("personId", personid).append("time", i);
+				FindIterable<Document> findItrable = collection.find(query1);
+				MongoCursor<Document> mongoCursor = findItrable.iterator();
+
+				while (mongoCursor.hasNext()) {
+					countOfCursor++;
+					mongoCursor.next();
+				}
+				
+				if (countOfCursor == 0) {
+					Document query = new Document("personId", personid);
+					collection.deleteMany(query);
+				}else if(countOfCursor > 1){
+					for(int j = 0;j<countOfCursor-1;j++){
+						Document query = new Document("personId", personid).append("time", i);
+						collection.deleteOne(query);
+					}
+				}
+			}
+		}
+		
+		
+		return;
+	}
+
 }
